9270a090d137e2d31c7eb245830c22c2f7bb9b00
Sapan Patibandha <patibandha@hotmail.com>
2021-09-03 22:23:11 +0530

    Check in 2,

--- SocketClient/Program.cs
+++ SocketClient/Program.cs
@@ -1,14 +1,77 @@
 ﻿using System;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace SocketClient
 {
+    public class MyMessage
+    {
+        public string StringProperty { get; set; }
+        public int IntProperty { get; set; }
+    }
+
     class Program
     {
+        static async Task SendAsync<T>(NetworkStream networkStream, T message)
+        {
+            var (header, body) = Encode(message);
+            await networkStream.WriteAsync(header, 0, header.Length).ConfigureAwait(false);
+            await networkStream.WriteAsync(body, 0, body.Length).ConfigureAwait(false);
+        }
 
+        static async Task<T> ReceiveAsync<T>(NetworkStream networkStream)
+        {
 
-        static void Main(string[] args)
+            var headerBytes = await ReadAsync(networkStream, 4);
+            var bodyLength = IPAddress.NetworkToHostOrder(BitConverter.ToInt32(headerBytes));
+
+            var bodyBytes = await ReadAsync(networkStream, bodyLength);
+
+            return Decode<T>(bodyBytes);
+        }
+
+        static (byte[] header, byte[] body) Encode<T>(T message)
+        {
+            var xs = new XmlSerializer(typeof(T));
+            var sb = new StringBuilder();
+            var sw = new StringWriter(sb);
+            xs.Serialize(sw, message);
+
+            var bodyBytes = Encoding.UTF8.GetBytes(sb.ToString());
+            var headerBytes = BitConverter.GetBytes(IPAddress.HostToNetworkOrder(bodyBytes.Length));
+
+            return (headerBytes, bodyBytes);
+        }
+
+        static T Decode<T>(byte[] body)
+        {
+            var str = Encoding.UTF8.GetString(body);
+            var sr = new StringReader(str);
+            var xs = new XmlSerializer(typeof(T));
+            return (T)xs.Deserialize(sr);
+        }
+
+        static async Task<byte[]> ReadAsync(NetworkStream networkStream, int bytesToRead)
+        {
+            var buffer = new byte[bytesToRead];
+            var bytesRead = 0;
+            while (bytesRead < bytesToRead)
+            {
+                var bytesReceived = await networkStream.ReadAsync(buffer, bytesRead, (bytesToRead - bytesRead)).ConfigureAwait(false);
+                if (bytesReceived == 0)
+                    throw new Exception("Socket Closed");
+                bytesRead += bytesReceived;
+            }
+            return buffer;
+        }
+
+        static void Print(MyMessage m) => Console.WriteLine($"MyMessage.IntProperty = {m.IntProperty}, MyMessage.StringProperty = {m.StringProperty}");
+
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Press Enter to Connect");
             Console.ReadLine();
@@ -19,18 +82,22 @@ namespace SocketClient
             socket.Connect(endpoint);
             var networkStream = new NetworkStream(socket, true);
 
-            var msg = "This is message";
+            var myMessage = new MyMessage
+            {
+                IntProperty = 404,
+                StringProperty = "Hello World"
+            };
 
-            var buffer = System.Text.Encoding.UTF8.GetBytes(msg);
+            Console.WriteLine("Sending");
+            Print(myMessage);
 
-            networkStream.Write(buffer, 0, buffer.Length);
-            var response = new byte[1024];
+            await SendAsync(networkStream, myMessage).ConfigureAwait(false);
 
-            var bytesRead = networkStream.Read(response, 0, response.Length);
+            var responseMsg = await ReceiveAsync<MyMessage>(networkStream).ConfigureAwait(false);
 
-            var responseStr = System.Text.Encoding.UTF8.GetString(response);
+            Console.WriteLine("Received");
+            Print(responseMsg);
 
-            Console.WriteLine($"Received : {responseStr}");
 
             Console.ReadLine();
         }
